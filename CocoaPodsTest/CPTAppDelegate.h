e0f50df46acbd5b10db5da94d5c89a09ce59bb6b
Анастасия Долгих <anastasiadolgih@Anastasias-Mac.local>
2014-05-25 01:45:09 -0700

    Initial Commit

---
+++ CocoaPodsTest/CPTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CPTAppDelegate.h
+//  CocoaPodsTest
+//
+//  Created by Анастасия Долгих on 5/25/14.
+//  Copyright (c) 2014 Anastasia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CocoaPodsTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CocoaPodsTest
+//
+//  Created by Анастасия Долгих on 5/25/14.
+//  Copyright (c) 2014 Anastasia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CPTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CPTAppDelegate class]));
+    }
+}
